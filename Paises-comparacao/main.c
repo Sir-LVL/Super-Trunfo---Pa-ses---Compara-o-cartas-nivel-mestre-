dcf1054838ee6742cd160cfee2d1ff6b828940da
Lucas Vladson C. de Lima <lucasvladsonhd@gmail.com>
2025-08-21 16:48:00 -0300

    add_menu

--- Paises-comparacao/main.c
+++ Paises-comparacao/main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int opcao;
+
+
+    printf("==================================\n");
+    printf("          MENU PRINCIPAL          \n");
+    printf("==================================\n");
+    printf("1 - Item 1\n");
+    printf("2 - Item 2\n");
+    printf("==================================\n");
+    printf("Escolha uma opcão: ");
+    scanf("&d", &opcao);
+    getchar();
+
+    switch (opcao){
+    case 1:
+        printf("teste switch");
+        break;
+    case 2:
+        printf("Teste 2");
+        break;
+    default:
+        printf("opção invalida, digite novamente");
+            }
     return 0;
 }
